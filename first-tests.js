19a3bc8035603c91506a88ce67dffef478058d06
shachem <sekki.hachem@outlook.com>
2022-08-28 02:27:55 +0100

    Write 3 test cases for orderTotal()

--- first-tests.js
+++ first-tests.js
@@ -1,1 +1,28 @@
 function orderTotal(order) {}
+
+if (
+  orderTotal({
+    items: [
+      { name: "Dragon food", price: 8, quantity: 1 },
+      { name: "Dragon cage (small)", price: 800, quantity: 1 },
+    ],
+  }) !== 808
+) {
+  throw new Error("Check fail: Happy path");
+}
+
+if (
+  orderTotal({
+    items: [{ name: "Dragon candy", price: 3 }],
+  }) !== 3
+) {
+  throw new Error("Check fail: No quantity specified");
+}
+
+if (
+  orderTotal({
+    items: [{ name: "Dragon candy", price: 2, quantity: 3 }],
+  }) !== 6
+) {
+  throw new Error("Check fail: Quantity");
+}
